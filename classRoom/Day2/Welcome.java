0c6ead274d29f83e97e38f3a2d1b17f1f81e91b1
Akshat303 <sri303akshat@gmail.com>
2026-08-27 21:41:02 +0530

    create different class file , file name and class name is difference withiut use public class

---
+++ classRoom/Day2/Welcome.java
@@ -0,0 +1,23 @@
+class Greeting
+{
+	public static void main(String args[]) //Entry point
+	{
+		System.out.println("Hello, Welcome everyone !!!");
+		System.out.println("this is my 2nd line ???" );
+	}
+}
+
+class SecondClass
+	{
+	public static void main(String args[])
+	{
+		System.out.println("This is 2nd class statemnt");
+	}
+	}
+class ThirdClass
+	{
+	
+	}
+class FourthClass
+	{
+	}
---
+++ practice/Day2/DemoTest1.java
@@ -0,0 +1,15 @@
+ class HomeWork{
+	public static void main (String args[])
+	{
+	System.out.println("Homework And practice program");
+	}
+}
+
+class Home2{}
+class home3{}
+class InsideHomeWork{
+	public static void main(String args[]){
+	System.out.println("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966");
+	}
+}
+class HomeTest4{}
