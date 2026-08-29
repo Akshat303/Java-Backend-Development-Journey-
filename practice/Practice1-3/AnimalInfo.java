c719f3019618dcd3a87a4bd6455f884e7e8dfeff
Akshat303 <sri303akshat@gmail.com>
2026-08-29 20:06:20 +0530

    more practice code

---
+++ practice/Practice1-3/AnimalInfo.java
@@ -0,0 +1,123 @@
+public class AnimalInfo{
+    public static void main (String [] ani){
+       System.out.println("| Animal | Type | Food | Habitat | Lifespan |") ;
+    }
+}
+class LionInfo{
+    public static void main (String [] Lion){
+        System.out.println("Animal- Lion");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Grassland");
+        System.out.println("Lifespan - 10-14 years");
+    }
+}
+class ElephantInfo{
+    public static void main (String [] Elephant){
+        System.out.println("Animal- Elephant");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Herbivore");
+        System.out.println("Habitat - Forest/Grassland");
+        System.out.println("Lifespan - 60-70 years");
+    }
+}
+
+class TigerInfo{
+    public static void main (String [] Tiger){
+        System.out.println("Animal- Tiger");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Forest");
+        System.out.println("Lifespan - 10-15 years");
+    }
+}
+
+class GiraffeInfo{
+    public static void main (String [] Giraffe){
+        System.out.println("Animal- Giraffe");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Herbivore");
+        System.out.println("Habitat - Savanna");
+        System.out.println("Lifespan - 20-25 years");
+    }
+}
+
+class DogInfo{
+    public static void main (String [] Dog){
+        System.out.println("Animal- Dog");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Omnivore");
+        System.out.println("Habitat - Domestic");
+        System.out.println("Lifespan - 10-13 years");
+    }
+}
+
+class CatInfo{
+    public static void main (String [] Cat){
+        System.out.println("Animal- Cat");
+        System.out.println("Type - Mammal");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Domestic");
+        System.out.println("Lifespan - 12-18 years");
+    }
+}
+
+class EagleInfo{
+    public static void main (String [] Eagle){
+        System.out.println("Animal- Eagle");
+        System.out.println("Type - Bird");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Mountains/Forests");
+        System.out.println("Lifespan - 20-30 years");
+    }
+}
+
+class PenguinInfo{
+    public static void main (String [] Penguin){
+        System.out.println("Animal- Penguin");
+        System.out.println("Type - Bird");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Antarctica");
+        System.out.println("Lifespan - 15-20 years");
+    }
+}
+
+class CrocodileInfo{
+    public static void main (String [] Crocodile){
+        System.out.println("Animal- Crocodile");
+        System.out.println("Type - Reptile");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Rivers/Lakes");
+        System.out.println("Lifespan - 50-70 years");
+    }
+}
+
+class SnakeInfo{
+    public static void main (String [] Snake){
+        System.out.println("Animal- Snake");
+        System.out.println("Type - Reptile");
+        System.out.println("Food - Carnivore");
+        System.out.println("Habitat - Forest/Grassland");
+        System.out.println("Lifespan - 10-30 years");
+    }
+}
+
+class FrogInfo{
+    public static void main (String [] Frog){
+        System.out.println("Animal- Frog");
+        System.out.println("Type - Amphibian");
+        System.out.println("Food - Insectivore");
+        System.out.println("Habitat - Wetlands");
+        System.out.println("Lifespan - 5-10 years");
+    }
+}
+
+class GoldfishInfo{
+    public static void main (String [] Goldfish){
+        System.out.println("Animal- Goldfish");
+        System.out.println("Type - Fish");
+        System.out.println("Food - Omnivore");
+        System.out.println("Habitat - Freshwater");
+        System.out.println("Lifespan - 10-15 years");
+    }
+}
