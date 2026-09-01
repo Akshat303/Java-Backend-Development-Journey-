e25eddf43e5c720be764a5da24f5e0a274313032
Akshat303 <sri303akshat@gmail.com>
2026-09-01 22:37:57 +0530

    add

---
+++ practice/Akshta-Practice/CmdInput.java
@@ -0,0 +1,8 @@
+class CMDInput{
+    public static void main (String[] args){
+        String name= args[0];
+        String location =args[1];
+        System.out.println("Welcome "+ name);
+        System.out.println(name + " is leave from " + location);
+    }
+}
