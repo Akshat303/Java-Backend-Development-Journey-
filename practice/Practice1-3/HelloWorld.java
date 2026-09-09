23824c66197692341e3f58469cf8874b07ccc24e
Akshat303 <sri303akshat@gmail.com>
2026-09-09 15:49:41 +0530

    add

--- practice/Practice1-3/HelloWorld.java
+++ practice/Practice1-3/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld{
 class MyInfo{
     public static void main (String [] myinfo){
         System.out.println("My Name is - Akshat Srivastava");
+		System.out.println("Java Developer);
         System.out.println("Education - B.tech in cse (2021)" );
         System.out.print("Address- Dhanbad, Jharkhand, 826001");
 
